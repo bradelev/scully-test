ce3e204caa9f495e1472db79757fd090c03b3238
Braulio De Leon <hi@brauliodeleon.com>
2021-03-27 23:33:09 -0300

    add scully

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'example',
+    pathMatch: 'full'
+  },
+  {
+    path: 'example',
+    loadChildren: () => 
+      import('./example/example.module').then(
+        (mod) => mod.ExampleModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
